6c4e80a3a71c39ee1fa358016aba579b0af8cbce
AbsentPlays <absentplays@gmail.com>
2026-09-22 00:55:47 -0400

    Re-Naming
    Change some file names around.

--- src/test/java/com/ClanKickProtector/ClanKickProtectorPluginTest.java
+++ src/test/java/com/ClanKickProtector/ClanKickProtectorPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.ClanKickProtector;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ClanKickProtectorPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ClanKickProtectorPlugin.class);
 		RuneLite.main(args);
 	}
 }
